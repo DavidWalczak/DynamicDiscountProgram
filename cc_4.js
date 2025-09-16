1576da796e27e4d2fa42a7e83a57c0e81becda17
David Walczak <dwalczak@usf.edu>
2025-09-16 14:25:59 -0400

    middle of additions, added and fixed a lot of code from everywhere. Updated the product list

--- cc_4.js
+++ cc_4.js
@@ -1,85 +1,98 @@
+/*Array of products and a log of each product and its information with a for each sequence*/
+
 let products= [
-    
     {
         name: "Bread",
-        category: "Grocery",
+        category: "grocery",
         price: 5.99,
         inventory: 12,
     },
     {
         name: "Cybernetic Bread",
-        category: "Electronic",
-        price: 5.99,
-        inventory: 12,
+        category: "electronic",
+        price: 506.99,
+        inventory: 5,
     },
     {
-        name: "",
+        name: "Bread Flavored T-Shirt",
         category: "apparel",
-        price: 5.99,
-        inventory: 12,
+        price: 16.99,
+        inventory: 75,
     }, 
     {
-        name: "Bleached Bread",
+        name: "Sponge like Bread",
         category: "household",
-        price: 5.99,
-        inventory: 12,
+        price: 3.99,
+        inventory: 30,
     }, 
     {
-        name: Bread,
-        category: Food,
-        price: 5.99,
-        inventory: 12,
+        name: "Fancier Bread",
+        category: "grocery",
+        price: 9.99,
+        inventory: 22,
     }
-]
+];
 
-console.log(products)
+products.forEach(products => {
+    console.log(products.name, " | ", products.category, " | ", products.price, " | ", products.inventory)
+});
 
-let customerDiscount= 0;
+console.log(" ")
+
 /*
-for .... of; 
-apply customer discounts based on category of product by cycling through the array
-and checking category
+This next part creates the customer discount variable, and then applies a standard discount amount based upon the item ordered using a for loop with a switch statement inside
+which is used to actually identify the item. 
+apply customer discounts based on category of product by cycling through the array and checking category
 */
-for (element= 0; element < 4; element++) {
-    switch (product[element][1]) {
-        case ("grocery" || "household"): 
-             customerDiscount= 10;
-        break;
-        case "apparel":
-            customerDiscount= 15;
-        break;
-        case "electronics":
-            customerDiscount= 20;
-        break;
 
+
+for (let product of products) {
+    switch (products.category) {
+        case ("grocery" || "household"): 
+            product.discountedPrice = product.price * .9;
+            break;
+        case "apparel":
+            product.discountedPrice = product.price * .85;
+            break;
+        case "electronics":
+            product.discountedPrice = product.price * .8;
+            break;
+        default:
+            product.discountedPrice = product.price;
     }
 };
 
-console.log("Current Customer Discount Based on Chosen Products:" + customerDiscount + "%")
+products.forEach(products => {
+    console.log(products.name, " | ", products.disountedPrice)
+});
 
 /* This is the Step  4 code for customer type and additon of more discounts */
-let customerType= senior;
+let customerType= "senior";
 
-if (customerType= (senior && student)) {
-    let customerDiscount= xxx+12;
-    console.log("Updated Customer Discount: " + customerDiscount + "%")
-} else if (customerType= student) {
-    let customerDiscount= xxx+5;
-    console.log("Updated Customer Discount: " + customerDiscount + "%")
-} else if (customerType= senior) {
-    let customerDiscount= xxx+7;
-    console.log("Updated Customer Discount: " + customerDiscount + "%")
-} else {
-    console.log ("No additional discounts. Discount Remains: " + customerDiscount+ "%")
-}
+function applyCustomerDiscount(total,customerTpe){
+    if (customerType== ("student")) {
+        return total * .95;
+    } else if (customerType== "senior") {
+        return total *.93;
+    } else {
+        return total;
+}};
 
 /*
-while loop for customers
+for loop to simulate customers
 */
-let customerCheckout = 3;
 
-while (customerChekcout.count > 0) {
-    
-    
-    customerCheckout--
-}
+let customerTypes=[
+    "regular", "student", "senior"
+];
+
+let customerCheckout= [
+    {
+        name: "Bob",
+        orderItemOne: "Bread",
+        orderItemOneQuantity: 2,
+        orderItemTwo: "Cybernetic Bread",
+        orderItemTwoQuantity: 1
+    }
+];
+
